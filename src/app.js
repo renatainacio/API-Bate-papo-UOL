76a2962aa35901379b11893b689d139e685ed024
Renata Inacio <revazgauska@gmail.com>
2023-06-29 14:41:53 -0300

    feat post user

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import Joi from "joi";
+import {MongoClient} from 'mongodb';
 
 
 const PORT = 5000;
@@ -9,14 +11,39 @@ app.use(express.json());
 app.use(cors());
 dotenv.config();
 
-const mongoClient = new MongoClient(process.env.DATABASE_URL);
-let db;
-mongoClient.connect()
-  .then(() => db = mongoClient.db())
-  .catch((err) =>  console.log(err.message));
+const schema = Joi.object({
+    username: Joi.string()
+        .required()
+        .min(1),
+})
 
-app.post("/participants", (req, res) => {
-    username = req.body.name;
+const mongoClient = new MongoClient(process.env.DATABASE_URL);
+try {
+  await mongoClient.connect()
+} catch (err) {
+  console.log(err.message)
+}
+const db = mongoClient.db();
+
+
+app.post("/participants", async (req, res) => {
+    const username = req.body.name;
+    const {error, value} = (schema.validate({username: username}));
+    if(error)
+        res.sendStatus(422);
+    try{
+        const resp = await db.collection('participants').findOne({name: username});
+        if(resp)
+            return res.sendStatus(409);
+        const user = await db.collection('participants').insertOne({
+            name: username,
+            lastStatus: Date.now()
+        });
+        res.sendStatus(201);
+    }catch(err){
+        console.log(err.message);
+        res.sendStatus(500);
+    }
 });
 
 app.get("/participants", (req, res) => {
